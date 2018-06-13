2cebb2608c894c9aee53d50bbc97266bece0e26b
MichailPerm <ApxaHreJl@gmail.com>
2018-06-13 23:07:38 +0500

    Sceleton ready

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,44 @@
-def load_urls4check(path):
-    pass
+import requests
+import tldextract
+import whois
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f',
+        '--filepath',
+        help='Path to file with urls',
+        required=True
+    )
+    return parser.parse_args()
+
+
+def load_urls4check(filepath):
+    with open(filepath, 'r') as f:
+        urls = f.readlines()
+        return urls
 
 def is_server_respond_with_200(url):
-    pass
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        return True
+    else:
+        return None
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_dict = whois.query(domain_name)
+    return domain_dict.expiration_date
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
+    urls = load_urls4check(args.filepath)
+    for url in urls:
+        if is_server_respond_with_200(url):
+            extract_result = tldextract.extract(url)
+            domain_name = '.'.join(
+                [extract_result.domain, extract_result.suffix]
+            )
+            exp_date = get_domain_expiration_date(domain_name)
+            print(exp_date)
