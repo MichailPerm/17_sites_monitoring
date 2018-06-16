4142c660fbe375a16326442e0c163f85255d1a83
Michail_Z <ApxaHreJl@gmail.com>
2018-06-16 23:25:12 +0500

    First release of sites monitor

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+This script checks state of sites in list, got from file.
+
+# Requirements
+
+Before launch this program, you need to setup packages from requerements.txt
+```commandline
+pip install -r requirements.txt
+```
+
+# Quickstart
+
+Launch in Windows commandline
+
+```commandline
+(lenv) E:\lproj\17_sites_monitoring>python check_sites_health.py -f <path to file>
+Site https://mail.google.com/mail/u/0 works correctly without a problem.
+Connection not established, or URL http://www.e2.su wrong!
+Site https://www.mail.ru works correctly without a problem.
+```
+
+Launch in Linux is the same.
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -2,6 +2,7 @@ import requests
 import tldextract
 import whois
 import argparse
+import datetime
 
 
 def get_args():
@@ -20,25 +21,49 @@ def load_urls4check(filepath):
         urls = f.readlines()
         return urls
 
+
 def is_server_respond_with_200(url):
-    response = requests.get(url)
+    try:
+        response = requests.get(url, allow_redirects=True)
+    except(
+        requests.exceptions.MissingSchema,
+        requests.exceptions.ConnectionError
+    ):
+        return None
     if response.status_code == requests.codes.ok:
         return True
     else:
         return None
 
+
 def get_domain_expiration_date(domain_name):
-    domain_dict = whois.query(domain_name)
-    return domain_dict.expiration_date
+    whois_dict = whois.whois(domain_name)
+    exp_date = whois_dict.expiration_date
+    if isinstance(whois_dict.expiration_date, list):
+        exp_date = whois_dict.expiration_date[0]
+    return exp_date
+
 
 if __name__ == '__main__':
     args = get_args()
+    current_date = datetime.datetime.now()
     urls = load_urls4check(args.filepath)
     for url in urls:
+        url = url.replace('\n', '')
         if is_server_respond_with_200(url):
-            extract_result = tldextract.extract(url)
+            data_extracted_from_url = tldextract.extract(url)
             domain_name = '.'.join(
-                [extract_result.domain, extract_result.suffix]
+                [
+                    data_extracted_from_url.domain,
+                    data_extracted_from_url.suffix
+                ]
             )
             exp_date = get_domain_expiration_date(domain_name)
-            print(exp_date)
+            if (exp_date - current_date) > datetime.timedelta(days=30):
+                print('Site {} works correctly without a problem.'.format(url))
+            else:
+                print('Site {} will stop it\'s work less than a month!'.format(
+                        url))
+
+        else:
+            print('Connection not established, or URL {} wrong!'.format(url))
